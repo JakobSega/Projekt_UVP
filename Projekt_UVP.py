d0586504c876584f47db5fa0d454906efa888eed
Jakob Šega <jakob.sega.13@gmail.com>
2023-06-03 17:27:03 +0200

    poskušam zaobiti zavrnitev automatiziramega dostopa

--- Projekt_UVP.py
+++ Projekt_UVP.py
@@ -2,26 +2,54 @@ import csv
 import os
 import requests
 import re
+import selenium
 
 Page_number = 1
 Car_number = 1
-Cars_url = f"https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&makeModelVariant1.makeId=9000&makeModelVariant1.modelId=30&pageNumber={page_number}&ref=srpNextPage&scopeId=C&sortOption.sortBy=relevance&refId=3e6d2e0e-8fd8-8a33-ceb3-956b170f5017"
+Cars_url = f"https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&makeModelVariant1.makeId=9000&makeModelVariant1.modelId=30&pageNumber={Page_number}&ref=srpNextPage&scopeId=C&sortOption.sortBy=relevance&refId=3e6d2e0e-8fd8-8a33-ceb3-956b170f5017"
 Cars_directory = "cars"
 Main_cars_filename = f"main_cars{Page_number}.html"
 Secondary_cars_filename = f"secondary_cars{Car_number}.html"
 
 
+#def download_url_as_string(url):
+#    """Funkcija kot argument sprejme url in poskusi vrniti vsebino te spletne
+#    strani kot niz. V primeru, da med izvajanje pride do napake vrne None.
+#    """
+#    try:
+#        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+#        page_content = requests.get(url, headers=headers)
+#        page_content.raise_for_status()
+#    except requests.exceptions.RequestException as e:
+#        print("Spletna stran trenutno ni dosegljiva:", e)
+#        return None
+#    return page_content.text
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
 def download_url_as_string(url):
-    """Funkcija kot argument sprejme url in poskusi vrniti vsebino te spletne
-    strani kot niz. V primeru, da med izvajanje pride do napake vrne None.
+    """Function to download and return the content of a web page as a string.
+    Returns None in case of any errors.
     """
     try:
-        headers = {"User-Agent": "Chrome/111.0.5563.111"}
-        page_content = requests.get(url)
-    except requests.exceptions.RequestException:
-        print("Spletna stran trenutno ni dosegljiva.")
+        options = Options()
+        options.add_argument("--headless")  # Run the browser in headless mode
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        time.sleep(5)  # Wait for the challenge to be solved or content to load
+        page_content = driver.page_source
+        driver.quit()
+
+        return page_content
+    except Exception as e:
+        print("An error occurred while accessing the webpage:", e)
         return None
-    return page_content.text
 
 
 def save_string_to_to_file(text, directory, filename):
@@ -56,7 +84,9 @@ def save_main_htmls(url_page_number, directory, filename_page_number, page_numbe
 def save_secondary_htmls(info_from_blocks, directory, filename_car_number, car_number):
     """Funkcija sprejme seznam slovarjev, ki vsebujejo povezave in shrani vsebino spletnih strani v datoteko "directory"/"filename_car_number"."""
     for i in info_from_blocks:
-        save_html(i["povezava"], directory, filename_car_number)
+        povezava = i["povezava"]
+        print(povezava)
+        save_html(povezava, directory, filename_car_number)
         car_number +=1
 
 
@@ -88,7 +118,7 @@ def make_all_main_blocks(directory, main_filename_page_number, page_number, numb
 
 
 def get_info_from_block(block):
-    """Funkcija iz niza za posamezn blok izlošči povezavo in id."""
+    """Funkcija iz niza za posamezn blok izlušči povezavo in id."""
     povezava = re.search(r'data-testid="no-top"><a class="link--muted no--text--decoration result-item" href="(https://suchen\.mobile\.de/fahrzeuge/details.*?)" data-listing-id="\d+"', block)
     id = re.search(r'data-testid="no-top"><a class="link--muted no--text--decoration result-item" href="https://suchen\.mobile\.de/fahrzeuge/details.*?" data-listing-id="(\d+)"', block)
     return {"id": id.group(1), "povezava": povezava.group(1)}
@@ -97,14 +127,23 @@ def get_info_from_block(block):
 
 
 def get_info_from_blocks(directory, main_filename_page_number, page_number, number=50):
+    """Funkcija iz seznama blokov izlušči seznam povezav in id-jev."""
     blocks = make_all_main_blocks(directory, main_filename_page_number, page_number, number=50)
     povezave_ids = [get_info_from_block(block) for block in blocks]
     return povezave_ids
 
-print(get_info_from_blocks(Cars_directory, Main_cars_filename, Page_number, 2))
+seznam = get_info_from_blocks(Cars_directory, Main_cars_filename, Page_number, 2)
 
 
-#def get_info_from_html():
+def secondary_html_to_block(directory, secondary_filename):
+    path = os.path.join(directory, secondary_filename)
+    vzorec = r''
+    with open(path, "r", encoding="UTF8") as doc:
+        str = doc.read()
+    return re.findall(vzorec, str, flags=re.DOTALL)
+
+print([seznam[3]])
+save_secondary_htmls([seznam[3]], Cars_directory, Secondary_cars_filename, Car_number)
 
 
 #def main(redownload=True, reparse=True):
